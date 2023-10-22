8326530685d3a925d68d0b15b8b3cf84cf4e79a2
mcubd <106980362+mcubd@users.noreply.github.com>
2023-10-22 07:13:29 +0600

    Update Index.py

--- Index.py
+++ Index.py
@@ -35,11 +35,14 @@ def read_itemm():
 	if "720p" in resdict.keys():
 		mid=resdict['720p']
 		print(resdict['720p'])
-		return f"{mid}&title={title}", 201
+		resp7=flask.Response(mid)
+		resp7.headers['okayy'] = 'yes'
+		return f"{mid}&title={title}"
 	else:
 		print(resdict['360p'])
 		low=resdict['360p']
 		print("360 bro")
+		resp3=flask.Response(low)
 		return f"{low}&title={title}",204
 				
 if __name__ == '__main__': app.run(debug=False)
